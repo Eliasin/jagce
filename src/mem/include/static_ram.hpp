87b6f7e16da09b787aa397c27049e0935575dc3a
Steven Pham <steven.pham.8.2000@gmail.com>
2019-11-13 15:01:16 -0500

    Rename getBytesAsType to readBytesAsType and change return to T const * instead of copy of type T
    Avoids a copy of arbitrarily large T

--- src/mem/include/static_ram.hpp
+++ src/mem/include/static_ram.hpp
@@ -12,8 +12,8 @@ namespace jagce {
 		size_t size() const override;
 		char readByte(size_t index) const override;
 		void writeByte(size_t index, char byte) override;
-		char* getBytes(size_t index, size_t num) const override;
-		void writeBytes(char* bytes, size_t num) override;
+		char* readBytes(size_t index, size_t num) const override;
+		void writeBytes(size_t index, char* bytes, size_t num) override;
 	private:
 		char arr[N];
 	};
