251e1754943d014694ced33b81796ac55f215e14
Steven Pham <steven.pham.8.2000@gmail.com>
2019-11-24 02:00:59 -0500

    Add enum for register names

---
+++ src/logic/include/register_names.hpp
@@ -0,0 +1,16 @@
+#ifndef JAGCE_REGISTER_NAMES
+#define JAGCE_REGISTER_NAMES
+
+namespace jagce {
+
+	enum class RegisterName {
+		AF, A,
+		BC, B, C,
+		DE, D, E,
+		HL, H, L,
+		SP,
+		PC
+	};
+
+}
+#endif
