d277c96d9f532e3b3a95ca04d956e6df2f8f09fd
Steven Pham <steven.pham.8.2000@gmail.com>
2019-11-24 16:11:41 -0500

    Change test section to be more general with instruction types

--- src/logic/tests/decoder_tests.cpp
+++ src/logic/tests/decoder_tests.cpp
@@ -7,18 +7,17 @@
 TEST_CASE("decoder produces correct events", "[logic], [decoder]") {
 	jagce::Decoder decoder{};
 
-	SECTION("load instructions") {
+	SECTION("single instruction decodes") {
 		auto opcode = GENERATE(0x7F, 0x78);
 		
-		std::map<uint8_t, jagce::LoadEvent> expectedEvents{
-			{ 0x7F, { {jagce::RegisterName::A}, {jagce::RegisterName::A} }},
-			{ 0x78, { {jagce::RegisterName::A}, {jagce::RegisterName::B} }}
+		std::map<uint8_t, jagce::Event> expectedEvents{
+			{ 0x7F, {jagce::LoadEvent{ {jagce::RegisterName::A}, {jagce::RegisterName::A} }} },
+			{ 0x78, {jagce::LoadEvent{ {jagce::RegisterName::A}, {jagce::RegisterName::B} }} }
 		};
 		
 		jagce::ByteStream bytes{};
 		bytes.add(opcode);
 
-		jagce::Event event = decoder.decodeEvent(bytes);
-		REQUIRE(std::get<jagce::LoadEvent>(event) == expectedEvents[opcode]);
+		REQUIRE(decoder.decodeEvent(bytes) == expectedEvents[opcode]);
 	}
 }
